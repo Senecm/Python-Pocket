ab2ffc8afc4ea875e134020469346df5d08414d0
Kevin <58762403+Senecm@users.noreply.github.com>
2020-09-09 21:25:00 +0930

    Add files via upload

---
+++ Factor Finder.py
@@ -0,0 +1,10 @@
+print("Welcome to factor finder\n")
+user_num = int(input("Please enter a number: "))
+#main function
+def find_factors(number):
+   for i in range(1, number + 1):
+       if number % i == 0:
+           print(i)
+find_factors(user_num)
+
+
