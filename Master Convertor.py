d10705dbf3c0925dcc9381044e27537176af18b9
Kevin <58762403+Senecm@users.noreply.github.com>
2020-09-09 17:58:41 +0930

    Opitimising strings
    Kite installed.

--- Master Convertor.py
+++ Master Convertor.py
@@ -14,43 +14,41 @@ def userExit():
             break
         else: 
             print("Invalid response... returning...")
-    
-s1 = sleep(1)
 while True:
-    print("Hello, choose from the list below for conversion")
-    s1
-    print("---1: Length")
-    print("---2: Weight and Volume")
-    print("---3: Time")
-    s1
-    print("pick one I dare you")
+    print("Hello, conversions are listed below:")
+    sleep(1)
+    print(
+        '''
+        1. Length
+        2. Weight
+        3. Time
+        '''
+    )
+    sleep(1)
     user_choice = int(input("choose (1/2/3): "))
-
-    if type(user_choice) != int:
-        print("Invalid type! Enter a number!")
-        cont = input("continue? Y/N")
-        if cont == "Y":
-            break
     #Length
     if user_choice == 1:
-        print("Length conversion options:")
-        print("")
-        print("mm to cm")
-        print("mm to m")
-        print("mm to km")
-        print("")
-        print("cm to mm")
-        print("cm to m")
-        print("cm to km")
-        print("")
-        print("m to mm")
-        print("m to cm")
-        print("m to km")
-        print("")
-        print("km to mm")
-        print("km to cm")
-        print("km to m")
-        print("")
+        print(
+            '''
+            Length conversion options:
+
+            mm to cm
+            mm to m
+            mm to km
+
+            cm to mm
+            cm to m
+            cm to km
+
+            m to mm
+            m to cm
+            m to km
+
+            km to mm
+            km to cm
+            km to m
+            '''
+        )
         #user input
         user_input = input("Select an option: ")
         def choiceOne(option):
@@ -113,7 +111,7 @@ while True:
     elif user_choice == 2:
         print(
             '''
-            Weight conversion options
+            Weight conversion options:
 
             mg to g
             mg to kg
@@ -158,24 +156,27 @@ while True:
         userExit()
     #Time 
     elif user_choice == 3:
-        print("Time conversion options:")
-        print("")
-        print("ms to s")
-        print("ms to m")
-        print("ms to h")
-        print("")
-        print("s to ms")
-        print("s to m")
-        print("s to h")
-        print("")
-        print("m to ms")
-        print("m to s")
-        print("m to hr")
-        print("")
-        print("hr to ms")
-        print("hr to s")
-        print("hr to m")
-        print("")
+        print(
+            '''
+            Time conversion options:
+
+            ms to s
+            ms to m
+            ms to hr
+
+            s to ms
+            s to m
+            s to hr
+
+            m to ms
+            m to s
+            m to hr
+
+            hr to ms
+            hr to s
+            hr to m
+            '''
+        )
         #user input
         user_input = input("Select an option: ")
         def choiceThree(option):
@@ -237,9 +238,10 @@ while True:
         #calling the function
         choiceThree(user_input)
         userExit()
-
-
-
-
+    else: 
+        print("Thats not a valid number!\n")
+        sleep(0.5)
+        print("Returning...")
+        sleep(0.5)
 
  
