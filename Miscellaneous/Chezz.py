464a4ac1cca80e3478158eb750057f4f1f8cab01
Kevin <58762403+Senecm@users.noreply.github.com>
2020-09-10 21:04:16 +0930

    Add files via upload

--- Miscellaneous/Chezz.py
+++ Miscellaneous/Chezz.py
@@ -1,9 +1,44 @@
-class Pieces:
-    def pawn(self):
-        squares = {
-            "row": 2,
-            "IsMultiple": True
-        }
+import turtle
 
-chessPieces = Pieces()
-print(chessPieces.pawn())
+wn = turtle.Screen()
+wn.title("Chezz by Kevin")
+wn.bgcolor("brown")
+wn.setup(width=500, height=500)
+#wn.tracer(0)
+
+#board
+def draw_board(x, y):
+    pen = turtle.Turtle()
+    pen.penup()
+    #pen.speed(3) #debugging
+    pen.goto(x, y)
+    pen.pendown()
+    pen.shape("turtle")
+    pen.forward(450)
+    pen.right(90)
+    pen.forward(450)
+    pen.right(90)
+    pen.forward(450)
+    pen.right(90)
+    pen.forward(460)
+    pen.penup()
+    #removing the turtle
+    pen.color("brown")
+draw_board(-235, 240)
+#pieces
+def draw_pawn(x, y):
+    pawn = turtle.Turtle()
+    #pawn.speed(0)
+    pawn.shape("square")
+    pawn.color("white")
+    #pawn.shapesize(stretch_wid=2, stretch_len=2)
+    pawn.penup()
+    pawn.goto(x, y)
+
+
+#while True:
+draw_pawn(50, -5)
+
+#main game loop
+while True:
+    wn.update()
