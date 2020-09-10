0c16c03826c122ad8974f62d9e393548f5f58013
Kevin <58762403+Senecm@users.noreply.github.com>
2020-09-10 14:56:49 +0930

    Added Google alternative

--- Lyric Finder.py
+++ Lyric Finder.py
@@ -1,11 +1,21 @@
-import webbrowser
-
 __author__ = "Kevin"
-__licsence__ = "unlicenced"
+__licsence__ = "unlicsenced"
+
+import webbrowser
+from time import sleep
 while True:
     user_input = input("Enter a name of a song: ")
     print("openin in new tab...")
     webbrowser.open('https://genius.com/search?q='+user_input, new=2)
+    user_choice = input("Are you satisfied with your result (Y/N): ")
+    if user_choice == "Y":
+        print("Great!")
+    elif user_choice == "N":
+        print("Sorry to hear that!\n")
+        sleep(0.8)
+        print("Rerouting and searching through google...")
+        sleep(0.8)
+        webbrowser.open('https://google.com/search?q='+user_input+'(song)', new=2)
     user_exit = input("Exit(Y/N)?")
     if user_exit == "Y":
         break
