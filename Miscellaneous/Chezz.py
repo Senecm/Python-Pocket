4a66dd5f1ba300dd70e896f1afd00a7a381dfbba
Kevin <58762403+Senecm@users.noreply.github.com>
2020-09-14 13:09:23 +0930

    Begun square functionality

--- Miscellaneous/Chezz.py
+++ Miscellaneous/Chezz.py
@@ -4,14 +4,14 @@ wn = turtle.Screen()
 wn.title("Chezz by Kevin")
 wn.bgcolor("brown")
 wn.setup(width=500, height=500)
-#wn.tracer(0)
+wn.tracer(0)
 
 #board
-def draw_board(x, y):
+def draw_board():
     pen = turtle.Turtle()
     pen.penup()
     #pen.speed(3) #debugging
-    pen.goto(x, y)
+    pen.goto(-235, 240)
     pen.pendown()
     pen.shape("turtle")
     pen.forward(450)
@@ -24,21 +24,47 @@ def draw_board(x, y):
     pen.penup()
     #removing the turtle
     pen.color("brown")
-draw_board(-235, 240)
+#squares
+def draw_square(x, y):
+    pen = turtle.Turtle()
+    pen.pensize(width=60)
+    pen.penup()
+    pen.goto(x, y)
+    pen.pendown()
+    pen.shape("square")
+    pen.color("white")
+    #pen.forward()
 #pieces
 def draw_pawn(x, y):
     pawn = turtle.Turtle()
+    pawn.penup()
+    pawn.goto(x, y)
     #pawn.speed(0)
     pawn.shape("square")
     pawn.color("white")
     #pawn.shapesize(stretch_wid=2, stretch_len=2)
-    pawn.penup()
-    pawn.goto(x, y)
-
-
-#while True:
-draw_pawn(50, -5)
-
+#main draw function
+def draw():
+    #drawing the board
+    draw_board()
+    #drawing the squares
+    draw_square(-225, 229)
+    #drawing the pawns
+    draw_pawn(-150, -130)
+    draw_pawn(-100, -130)
+    draw_pawn(-50, -130)
+    draw_pawn(0, -130)
+    draw_pawn(50, -130)
+    draw_pawn(100, -130)
+    draw_pawn(150, -130)
+    draw_pawn(200, -130)
+    #drawing the rooks
+    #drawing the knights
+    #drawing the bishops
+    #drawing the king
+    #drawing the queen
+#calling the main function
+draw()
 #main game loop
 while True:
     wn.update()
