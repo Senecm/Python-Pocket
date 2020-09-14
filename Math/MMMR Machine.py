dab226f65e41f132b6429d0acf5a194d8ddf674b
Kevin <58762403+Senecm@users.noreply.github.com>
2020-09-14 15:07:03 +0930

    Begun

--- Math/MMMR Machine.py
+++ Math/MMMR Machine.py
@@ -2,4 +2,11 @@ __author__ = "Kevin"
 __liscence__ = "unliscenced"
 
 print("Welcome to the Mean Median Mode and Range machine!")
-input("")
+user_input = int(input("Enter numbers in the form of A, B, C: "))
+user_inpu = [user_input]
+print(sum(user_input))
+
+#for i in user_input:
+#    total = user_input[i] + user_input[i]
+#    print(f"Mean: {total}")
+#
